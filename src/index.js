04f29f3903ec4af96fea95544944b37e4a43acc4
Ivan Laver <ereyzer142@gmail.com>
2021-05-29 01:24:19 +0300

    основа готова тепер понтіфай

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './sass/main.scss';
-fetch('https://restcountries.eu/rest/v2/all?fields=name;capital;currencies').then(responsive => responsive.json()).then(console.log);
+import countryCardTeml from './templates/countryCardTeml.hbs';
+import _ from '../node_modules/lodash/lodash';
+import fetchCountries from './js/fetchCountries';
+
+
+const refs ={
+    input: document.querySelector('[data-input="searchQuery"]'),
+    container: document.querySelector('.js-container')
+};
+    console.log(refs.container);
+    
+    refs.input.addEventListener('input', _.debounce(getInputValue , 1000));
+
+    function getInputValue(e) {
+        // console.log(e.target.value);
+    const searchQuery = e.target.value;  
+        fetchCountries(searchQuery).then(r=>makeCards(r));
+    }
+
+
+    function makeCards(ArrOfCountries) {
+        console.log('make',  ArrOfCountries);
+       const test = ArrOfCountries.map((card)=> card.name);
+       console.log(test);
+       console.log(typeof ArrOfCountries)
+       console.log(countryCardTeml(ArrOfCountries));
+        refs.container.insertAdjacentHTML( 'beforeend', countryCardTeml(ArrOfCountries));
+    }
+
+
+
