7b7af94b009865fa823b71cb6f8fd9be8501318b
Ivan Laver <ereyzer142@gmail.com>
2021-05-29 15:21:13 +0300

    gjghfdbd

--- src/index.js
+++ src/index.js
@@ -1,32 +1,77 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+
+import PNotify, { notice, info, alert, success, error } from '../node_modules/@pnotify/core';
 import countryCardTeml from './templates/countryCardTeml.hbs';
 import _ from '../node_modules/lodash/lodash';
 import fetchCountries from './js/fetchCountries';
+// import { alert, defaultModules } from '@pnotify/core';
+// import * as PNotifyMobile from '@pnotify/mobile';
+
+console.log('харить така хуйня')
+// defaultModules.set(PNotifyMobile, {});
 
 
 const refs ={
     input: document.querySelector('[data-input="searchQuery"]'),
     container: document.querySelector('.js-container')
 };
-    console.log(refs.container);
+    
     
     refs.input.addEventListener('input', _.debounce(getInputValue , 1000));
 
+
     function getInputValue(e) {
-        // console.log(e.target.value);
-    const searchQuery = e.target.value;  
-        fetchCountries(searchQuery).then(r=>makeCards(r));
+        const searchQuery = e.target.value;  
+
+        fetchCountries(searchQuery).then((r)=>{
+
+            if(r.length > 10){
+                console.log('>10')
+                alert({
+                    text: 'повна хуйня!'
+                })
+                return null;
+            }          
+            console.log('<10')  
+            return r;
+        })
+        .then(makeCards)
+        .catch((r)=>{
+            console.log('work r= ', r);
+        //    return name();
+        }
+        )
+            
     }
 
+    // function name(params) {
+    //   return  
+    // }
+    // function reject(r) {
+    //     r = null
+    //     return r;
+
+    // }
 
     function makeCards(ArrOfCountries) {
-        console.log('make',  ArrOfCountries);
-       const test = ArrOfCountries.map((card)=> card.name);
-       console.log(test);
-       console.log(typeof ArrOfCountries)
-       console.log(countryCardTeml(ArrOfCountries));
         refs.container.insertAdjacentHTML( 'beforeend', countryCardTeml(ArrOfCountries));
     }
 
+    // const promise = new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //       reject('success!');
+    //     }, 2000);
+    //   });
+    //   const onResolve = data => {
+    //     console.log('INSIDE promise.then - onResolve');
+    //     console.log(data); // "Data passed into resolve function :)"
+    //   };
+      
+    //   const onReject = error => {
+    //     console.log('INSIDE promise.then - onReject');
+    //     console.log(error); // "Error passed into reject function :("
+    //   };
 
+    //   promise.then(onResolve, onReject)
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,19 @@
+
+
 const BASE_URL =  'https://restcountries.eu/rest/v2/';
 
 export default function fetchCountries(searchQuery) {
     return fetch(`${BASE_URL}name/${searchQuery}`)
     .then(response => response.json())
+    // .then(kurva)
+    .catch((r)=>console.log("lot of 10"));
 }
 
+// fetchCountries.then((response)=>{
+//     return Promise((resolve, reject)=>{
+//         if(response.length > 10){
+//             reject()
+//         }
+//         resolve()
+//     })
+// })
