6fa2e084951f7efbc16086d4926c7d415e2a5ca2
Ivan Laver <ereyzer142@gmail.com>
2021-05-30 23:13:35 +0300

    думаю готово

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
 import './sass/main.scss';
 import '@pnotify/core/dist/BrightTheme.css';
 
+import PNotify, { notice, info, alert, success, error } from '@pnotify/core';
+
 import countryCardTeml from './templates/countryCardTeml.hbs';
+import listTemplate from './templates/list.hbs';
+
 import _ from '../node_modules/lodash/lodash';
 import fetchCountries from './js/fetchCountries';
 
 const refs ={
     input: document.querySelector('[data-input="searchQuery"]'),
-    container: document.querySelector('.js-container')
+    container: document.querySelector('.js-container'),
+    list: document.querySelector('.js-list')
 };
-     
-    refs.input.addEventListener('input', _.debounce(getInputValue , 500));
+
+    let countries = [];
+    refs.list.addEventListener('click', onItemsClick)
+    refs.input.addEventListener('input', _.debounce(getInputValue, 500));
 
     function getInputValue(e) {
-        const searchQuery = e.target.value;  
-        if (!searchQuery) {
-            return;
-        }
+        const searchQuery = e.target.value.trim();  
+        if (!searchQuery)return;
+        // const debounced = _.debounce(makeCards, 500);
         refs.container.innerHTML = '';
+        refs.list.innerHTML = '';
         fetchCountries(searchQuery)
-        .then(makeCards)
-        .catch(console.log)
+        .then(testRes)
+        .then(makeList)
+        .catch(errorEntries)
+    }
+
+    function testRes(res) {
+        if(res.length > 10)throw new Error('We have more ten results. Please enter more characters!');
+
+        if(res.status === 404)throw new Error('no EXIST');
+
+         return res;
+    }
+
+    function errorEntries(text) {
+        return   error({
+            text: `${text}`
+        });
+    }
+
+    function makeList(list) {
+        refs.list.innerHTML = `${listTemplate(list)}`;
+        countries = [...list];
+        return list;
     }
 
     function makeCards(ArrOfCountries) {
@@ -30,4 +58,18 @@ const refs ={
         refs.container.insertAdjacentHTML( 'beforeend', countryCardTeml(ArrOfCountries));
     }
 
+    function onItemsClick(e) {
+        const country = e.target.textContent
+        refs.input.value = country;
+        makeCards(countries.reduce((acc,el)=>{
+            if(el.name === country){
+                acc.push(el);
+            }
+        return acc;},[]))
+        refs.input.value = '';
+        refs.list.innerHTML = '';
+        return;
+    }
+
+    
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,30 +1,76 @@
 // import '@pnotify/core/dist/BrightTheme.css';
-import PNotify, { notice, info, alert, success, error } from '@pnotify/core';
+
 import { functions, reject } from 'lodash';
+
 
 const BASE_URL =  'https://restcountries.eu/rest/v2/';
 
 
 export default function fetchCountries(searchQuery) {
-
-    const fetchPromise = () => new Promise((resolve, reject)=>{
-         fetch(`${BASE_URL}name/${searchQuery}`)
-        .then(response => response.json())
-        .then((response)=>{
-            if(response.length > 10){
-                return reject('We have more ten results. Please enter more characters!');
-            }
-            if(response.status === 404){
-                return reject('no EXIST');
-            }   
-            resolve(response);   
-        }).catch(errorEntries)
-    })
-    return fetchPromise().catch(errorEntries)
-}
+        return fetch(`${BASE_URL}name/${searchQuery}`)
+        .then(response => response.json()).catch(console.error)
+    }
 
 function errorEntries(text) {
     return   error({
         text: `${text}`
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const BASE_URL =  'https://restcountries.eu/rest/v2/';
+
+
+// export default function fetchCountries(searchQuery) {
+
+//     const fetchPromise = () => new Promise((resolve, reject)=>{
+//         return fetch(`${BASE_URL}name/${searchQuery}`)
+//         .then(response => response.json())
+//         .then((response)=>{
+//             console.log('first response');
+//             if(response.length > 10)return reject('We have more ten results. Please enter more characters!');
+//             console.log('second response');
+//             if(response.status === 404)return reject('no EXIST');
+//             console.log('third response');
+//              resolve(response);
+//             console.log('for response', response);
+//         }).catch(errorEntries)
+//     })
+//     return fetchPromise().catch(errorEntries)
+// }
+
+// function errorEntries(text) {
+//     return   error({
+//         text: `${text}`
+//     });
+// }
